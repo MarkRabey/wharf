4b01cfd493d56b4d9906c8ebb23d0d24a2022927
Mark Rabey <mark@markrabey.com>
2018-04-09 11:04:43 -0400

    removed react code

--- src/renderer/components/Command.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-import * as React from 'react';
-export declare class Command extends React.Component {
-    props: any;
-    constructor(props: any);
-    renderProcess(): void;
-    render(): JSX.Element;
-}
--- src/renderer/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const ReactDOM = require("react-dom");
-const App_1 = require("./components/App");
-// Render components
-ReactDOM.render(React.createElement(App_1.default, null), document.getElementById('app'));
-//# sourceMappingURL=main.js.map
